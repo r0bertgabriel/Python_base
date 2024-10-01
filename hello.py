b68ab6b6d56669a84531b63fbf579ec8d86118a3
r0bert <robertdsgabriel@gmail.com>
2024-10-01 13:35:31 -0300

    update

---
+++ hello.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+    
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+print("Hello from python-base!")
+print(3+1)
+
+
