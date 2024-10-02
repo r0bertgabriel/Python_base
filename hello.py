f8eaeadeb2dc3b4d6e824d95724ee2b5d3eed49d
r0bert <robertdsgabriel@gmail.com>
2024-10-01 23:02:53 -0300

    implementamos o hello world multi-linguas

--- hello.py
+++ hello.py
@@ -20,11 +20,19 @@ __version__ = "0.0.1"
 __author__ = "r0bert"
 __license__ = "Unlicense"
 
-current_language = "en_US"
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
 
 msg = 'Hello, World!'
 
 if current_language == "pt_BR":
     msg = "Olá, mundo!"
 
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
 print(msg)
