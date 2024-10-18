11b78729d20d94bc9112f7f13736f707bec1ffe0
r0bert <robertdsgabriel@gmail.com>
2024-10-18 10:23:55 -0300

    update version

--- tabuada.py
+++ tabuada.py
@@ -5,17 +5,18 @@ Tabuada do 1
 
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __author__ = "r0bert"
 
-# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Iterable (percorriveis)
 numeros = list(range(1, 11))
 
-# Iterable (percorriveis)
-
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("------------------------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    bloco = ""
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}\n"))
+    print("#" * 18)
