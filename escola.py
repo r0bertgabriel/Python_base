4a4404560eff06fa668d6d3773e2f85d9dfd79d0
r0bert <robertdsgabriel@gmail.com>
2024-10-28 21:33:32 -0300

    adc escola-script

---
+++ escola.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+Imprimir a lista de crianças agrupadas por sala que frequentam cada uma das
+atividades
+"""
+
+__version__ = "0.1.0"
+
+# dados
+sala1 = ["Erick", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erick", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erick", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
