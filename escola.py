54ba9976ed65718ee8e3a13d1e428cb1909077eb
r0bert <robertdsgabriel@gmail.com>
2024-10-28 21:42:01 -0300

    update

--- escola.py
+++ escola.py
@@ -13,3 +13,16 @@ sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
 aula_ingles = ["Erick", "Maia", "Joana", "Carlos", "Antonio"]
 aula_musica = ["Erick", "Carlos", "Maria"]
 aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+# Listar alunos em cada atividade por sala
+aula_ingles_sala1 = []
+aula_ingles_sala2 = []
+
+for aluno in aula_ingles:
+    if aluno in sala1:
+        aula_ingles_sala1.append(aluno)
+    elif aluno in sala2:
+        aula_ingles_sala2.append(aluno)
+
+print("Inglês sala 1:", aula_ingles_sala1)
+print("Inglês sala 2:", aula_ingles_sala2)
